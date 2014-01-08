6476c144f7b96d63958fff31e2ace5dd3a7bd1af
piyushanchanto <piyush.awasthi@anchanto.com>
2014-01-08 11:27:52 +0530

    new product list added

---
+++ project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '64aa693464354a6ad0a83c155cc535c77e6437a635ddc7e63c09f02fe5283edd64ef307ded1f98f43a091b26632866749355555a6b2362f9d30c30833eb59b25'
